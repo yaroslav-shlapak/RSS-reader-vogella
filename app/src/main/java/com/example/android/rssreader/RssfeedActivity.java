ec7c5c410d4679261caa62076194f51344e377ff
Yaroslav <yarosalv.shlapak@gmail.com>
2015-05-23 16:06:49 +0300

    Dynamic fragments operation

--- app/src/main/java/com/example/android/rssreader/RssfeedActivity.java
+++ app/src/main/java/com/example/android/rssreader/RssfeedActivity.java
@@ -1,28 +1,39 @@
 package com.example.android.rssreader;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.app.ListFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class RssfeedActivity extends Activity implements MyListFragment.OnItemSelectedListener{
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rssfeed);
+        if (!getResources().getBoolean(R.bool.dual_pane)) {
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.add(R.id.listcontainer, new ListFragment());
+            ft.commit();
+        }
     }
 
     @Override
     public void onRssItemSelected(String link) {
-        DetailFragment fragment = (DetailFragment) getFragmentManager()
-                .findFragmentById(R.id.detailFragment);
-        if (fragment != null && fragment.isInLayout()) {
+        FragmentManager fm = getFragmentManager();
+
+        if (getResources().getBoolean(R.bool.dual_pane)) {
+            DetailFragment fragment = (DetailFragment) fm.findFragmentById(R.id.detailFragment);
             fragment.setText(link);
         } else {
-            Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
-            intent.putExtra(DetailActivity.EXTRA_URL, link);
-            startActivity(intent);
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.detailscontainer, new ListFragment());
+            ft.remove(fm.findFragmentById(R.id.listcontainer));
+            ft.addToBackStack(null);
+            ft.commit();
         }
     }
 
